a1fc232c67e54631abf86cbb71f9599021758f76
Austin Gill <notgnoshi@gmail.com>
2019-09-05 21:58:02 -0600

    Replace IPython.display.Code._repr_html_ to fix bug
    Swap out the private(ish) _repr_html_ method with my own that adds the
    appropriate CSS class to syntax highlight the source code in Jupyter
    Lab.

--- haikulib/utils/notebook.py
+++ haikulib/utils/notebook.py
@@ -2,10 +2,9 @@ import importlib
 import inspect
 
 import IPython
-import pygments
 
 
-def display_source(module: str, function: str) -> IPython.core.display.HTML:
+def display_source(module: str, function: str) -> IPython.display.Code:
     """Display the source of the provided function in a notebook.
 
     :param module: The module containing function. Must be importable.
@@ -14,10 +13,18 @@ def display_source(module: str, function: str) -> IPython.core.display.HTML:
     __module = importlib.import_module(module)
     __methods = dict(inspect.getmembers(__module, inspect.isfunction))
 
-    return IPython.core.display.HTML(
-        pygments.highlight(
-            inspect.getsource(__methods[function]),
-            pygments.lexers.PythonLexer(),
-            pygments.formatters.HtmlFormatter(full=True),
+    def _jupyterlab_repr_html_(self):
+        from pygments import highlight
+        from pygments.formatters import HtmlFormatter
+
+        fmt = HtmlFormatter()
+        style = "<style>{}\n{}</style>".format(
+            fmt.get_style_defs(".output_html"), fmt.get_style_defs(".jp-RenderedHTML")
         )
-    )
+        return style + highlight(self.data, self._get_lexer(), fmt)
+
+    # Replace the _repr_html_() method with our own that also adds the `jp-RenderedHTML` class
+    # to fix https://github.com/jupyterlab/jupyterlab/issues/6376.
+    IPython.display.Code._repr_html_ = _jupyterlab_repr_html_
+    # Mmmm. Slimy.
+    return IPython.display.Code(data=inspect.getsource(__methods[function]), language="python3")
