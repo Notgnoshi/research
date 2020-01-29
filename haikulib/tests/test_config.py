e29de15a9f0ee4445a7603eec623d608cfb2ba33
Austin Gill <notgnoshi@gmail.com>
2020-01-29 15:19:53 -0700

    Add unit test stub for testing config validation
    See https://github.com/Notgnoshi/research/issues/46

---
+++ haikulib/tests/test_config.py
@@ -0,0 +1,10 @@
+import unittest
+
+import commentjson
+import jsonschema
+
+# TODO: Add nice wrappers to haikulib.config?
+
+class TestConfigValidation(unittest.TestCase):
+    # TODO: https://github.com/Notgnoshi/research/issues/46
+    pass
