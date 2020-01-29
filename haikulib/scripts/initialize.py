34edae86313100de2c531cdc23581c0e1dc9dfec
Austin Gill <notgnoshi@gmail.com>
2020-01-29 15:15:14 -0700

    Remove sys.path abomination.
    Dockerfile adds the workspace to PYTHONPATH.

--- haikulib/scripts/initialize.py
+++ haikulib/scripts/initialize.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
 """Prepare the haiku CSV dataset."""
-import pathlib
-import sys
-
-REPO_DIR = pathlib.Path(__file__).parent.parent.parent
-DATA_DIR = REPO_DIR / "data"
-
-# Add repository root directory to path so that haikulib.utils.data is importable.
-sys.path.append(str(REPO_DIR))
-
 from haikulib.data import get_df
-from haikulib.data.initialization import init_csv, init_nltk
+from haikulib.data.initialization import init_csv
 
 if __name__ == "__main__":
     # Downloading the NLTK data is done in the Docker image.
     # init_nltk()
     init_csv()
-    df = get_df()
-    print(df.tail())
+    print(get_df().tail())
